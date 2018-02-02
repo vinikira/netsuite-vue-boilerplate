82979fe6c95fd7235f2300eca8d618afbf42f86f
Vinícius Simões <Vincius Simes>
2018-02-02 11:15:57 -0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="root">
+    <h1>Hello World!</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+      Name: 'root',
+      data () {
+          return {
+              msg: 'Hello NetSuite!'
+          }
+      }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inlineSource: '.(js|css)$',
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+        new HtmlWebpackInlineSourcePlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
